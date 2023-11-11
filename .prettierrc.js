86c0d7e9c6d72ca00a9c39f84c7b6bdd48aab13c
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2023-11-10 23:30:43 -0300

    Add eslint and prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    bracketsSpacing: true,
+    singleQuote: true,
+    tabWith: 2,
+    useTabs: false,
+    trailingComma: "none",
+    semi: false
+}
---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// metro.config.js
+module.exports = {
+    resolver: {
+      sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json', 'md'], // Asegúrate de incluir todas las extensiones que estás utilizando en tu proyecto
+    },
+  };
+  
