eb54749ed6d34431f5f08f771b362b95d99e3ab4
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2023-11-14 08:46:18 -0300

    Creating the upcoming weather component

---
+++ src/components/UpcomingWeather/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+import styles from "./styles";
+
+const UpcomingWeather = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Upcoming Weather</Text>
+        </SafeAreaView>
+    );
+}
+
+export default UpcomingWeather;
