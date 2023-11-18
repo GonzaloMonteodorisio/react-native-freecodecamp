8184ab0f3ba9f23eab1e8c3cf694fb8f46f1cc90
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2023-11-18 13:23:09 -0300

    Introducing lists

--- src/components/UpcomingWeather/index.js
+++ src/components/UpcomingWeather/index.js
@@ -1,12 +1,45 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, FlatList, View } from "react-native";
+import { Feather } from '@expo/vector-icons';
+
+import { DATA } from "./weatherData";
 
 import styles from "./styles";
+
+const Item = (props) => {
+    const { dt_txt, min, max, condition } = props;
+    return (
+        <View style={styles.item}>
+            <Feather
+                name="sun"
+                size={50}
+                color={"white"}
+            />
+            <Text style={styles.date}>{dt_txt}</Text>
+            <Text style={styles.temp}>{min}</Text>
+            <Text style={styles.temp}>{max}</Text>
+        </View>
+    );
+}
+
+const renderItem = ({ item }) => (
+    <Item
+        condition={item.weather[0].main}
+        dt_txt={item.dt_txt}
+        min={item.main.temp_min}
+        max={item.main.temp_max}
+    />
+);
 
 const UpcomingWeather = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Text>Upcoming Weather</Text>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.dt_txt}
+            />
         </SafeAreaView>
     );
 }
--- src/components/UpcomingWeather/styles.js
+++ src/components/UpcomingWeather/styles.js
@@ -1,8 +1,28 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
 
 const styles = StyleSheet.create({
     container:{
-        flex: 1
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+        backgroundColor: 'red'
+    },
+    item: {
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        borderWidth: 5,
+        backgroundColor: 'pink'
+    },
+    temp: {
+        color: 'white',
+        fontSize: 20
+    },
+    date: {
+        color: 'white',
+        fontSize: 15
     }
 });
 
