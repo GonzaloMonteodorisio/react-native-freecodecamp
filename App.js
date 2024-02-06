66d16dc41f8b0dcfa6831adf3f05d2ba22d18a6d
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2024-02-06 20:28:21 -0300

    Getting user Location

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { 
   ActivityIndicator, 
   View, 
   StyleSheet,
 } from "react-native"; 
+import * as Location from 'expo-location';
 import { NavigationContainer } from "@react-navigation/native";
 
 import Tabs from "./src/components/Tabs";
@@ -11,22 +12,56 @@ import Tabs from "./src/components/Tabs";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
 
-  if (loading) {
-    return (
-      <View style={styles.container}>
+  console.info('Location - App: ', Location);
+
+  useEffect(() => {
+    (async() => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      console.info('status - App!!!!!!!!!!!!!!!!!!!!!!: ', status);
+
+      /*
+      if (status !== 'granted') {
+        setError('Permission to access location was denied');
+        console.info('Error: Permission to access location was denied');
+        return;
+      }
+      */
+
+      try {
+        let location = await Location.getCurrentPositionAsync();
+        console.info('location - App!!!!!!!!!!!!!!!!!!!!!!: ', location);
+        setLocation(location);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error getting location:', error);
+        setError('Error getting location');
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    })()
+  }, []);
+
+  if (location) {
+    console.info('location - App: ', location);
+  }
+
+  return (
+    <View style={styles.container}>
+      {loading ? (
         <ActivityIndicator
           size="large"
           color="blue"
         />
-      </View>
-    )
-  }
-
-  return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+      ) : (
+        <NavigationContainer>
+          <Tabs />
+        </NavigationContainer>
+      )}
+    </View>
   );
 };
 
